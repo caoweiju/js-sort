0fc1924fd72b6f4f117b592b5a2cd65103337403
weiju.cao <caoweiju@foxmail.com>
2019-02-24 16:52:05 +0800

    update:更新堆排序

--- src/dui/index.js
+++ src/dui/index.js
@@ -1,6 +1,21 @@
 /*
  * @Author: weiu.cao
  * @Date: 2019-02-21 17:38:11
- * @Last Modified by:   weiju.cao
- * @Last Modified time: 2019-02-21 17:38:11
+ * @Last Modified by: weiju.cao
+ * @Last Modified time: 2019-02-24 16:51:48
 */
+
+/**
+ * 堆排序
+ * @param {*} needSortArray 
+ */
+function Heap_Sort(needSortArray=[]) {
+    const isArray = Object.prototype.toString.call(needSortArray) === '[object Array]',
+        isIntArray = needSortArray.every( item => typeof item === 'number');
+    if(!isArray || !isIntArray) { // 校验整数数组
+        return needSortArray;
+    }
+
+    
+    return needSortArray;
+}
