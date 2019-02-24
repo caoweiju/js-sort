c12cf4059dde6bb4bf1ccb14d9d616f26a801f6c
weiju.cao <caoweiju@foxmail.com>
2019-02-24 12:47:39 +0800

    docs:修改文档归并排序

--- src/guibing/index.js
+++ src/guibing/index.js
@@ -1,6 +1,7 @@
 /*
  * @Author: weiu.cao
  * @Date: 2019-02-21 17:38:18
- * @Last Modified by:   weiju.cao
- * @Last Modified time: 2019-02-21 17:38:18
+ * @Last Modified by: weiju.cao
+ * @Last Modified time: 2019-02-24 12:47:19
 */
+
